d131ee9002727403bd17ed2f91e70d41bea37ad1
xiaoroubao1996 <13816257947@163.com>
2019-04-12 18:22:54 +0800

    DAOQuestionnaire

---
+++ src/Model/DAOQuestionnaire.java
@@ -0,0 +1,194 @@
+package Model;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DAOQuestionnaire implements DAOInterface<Questionnaire> {
+    private ResultSet result;
+
+    @Override
+    public Questionnaire selectByID(int id){
+        Connection conn = null;
+        Statement stmt = null;
+        Questionnaire questionnaire = null;
+        try {
+            conn = SQL.getSQLConnection();
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM questionnaire WHERE id = "+ id;
+            result = stmt.executeQuery(sql);
+            if (result.next()){
+                questionnaire = new Questionnaire(
+                        Integer.valueOf(result.getString("id")),
+                        result.getString("subject"),
+                        Constant.STATUS.valueOf(result.getString("status"))
+                );
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return questionnaire;
+    }
+
+    @Override
+    public ArrayList<Questionnaire> selectAll() {
+        Connection conn = null;
+        Statement stmt = null;
+        ArrayList<Questionnaire> questionnaires = new ArrayList<Questionnaire>();
+        try {
+            conn = SQL.getSQLConnection();
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM questionnaire";
+            result = stmt.executeQuery(sql);
+            while (result.next()){
+                Questionnaire questionnaire = new Questionnaire(
+                        Integer.valueOf(result.getString("id")),
+                        result.getString("subject"),
+                        Constant.STATUS.valueOf(result.getString("status"))
+                );
+                questionnaires.add(questionnaire);
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return questionnaires;
+    }
+
+    @Override
+    public void add(Questionnaire questionnaire) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = SQL.getSQLConnection();
+            stmt = conn.createStatement();
+            String sql;
+            sql = "INSERT INTO Questionnaire (subject) VALUES ('"+ questionnaire.getSubject()+"')";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void update(Questionnaire questionnaire) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = SQL.getSQLConnection();
+            stmt = conn.createStatement();
+            String sql;
+            sql = "UPDATE `questionnaire` SET `subject`='"+ questionnaire.getSubject() +
+                    "',`status`= `"+ questionnaire.getStatus() +
+                    "` WHERE `id`='"+ questionnaire.getId() +"'";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void delete(Questionnaire questionnaire) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = SQL.getSQLConnection();
+            stmt = conn.createStatement();
+            String sql;
+            sql = "DELETE FROM `questionnaire`\n" +
+                    "WHERE `id` = '"+questionnaire.getId()+"'";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
--- src/Model/Questionnaire.java
+++ src/Model/Questionnaire.java
@@ -7,14 +7,21 @@ public class Questionnaire {
     private String subject;
     private Constant.STATUS status;
 
-    /*public void Questionnaire() {
+    public void Questionnaire() {
     	this.id = null;
     	this.subject = null;
     	status = ACTIVE;
-    }*/
+    }
     
     public Questionnaire(String subject) {
     	this.subject = subject;
+    }
+
+
+    public Questionnaire(int id, String subject, Constant.STATUS status) {
+        this.subject = subject;
+        this.id = id;
+        this.status = status;
     }
     
     public void setId(Integer id) {
